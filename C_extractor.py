373621b21ac16ab88a4fd149e46789527c54613c
Charlie-Toro <charles_toro@mail.com>
2017-06-04 21:40:37 -0400

    practices exceptions

---
+++ C_extractor.py
@@ -0,0 +1,16 @@
+# Contact Extractor
+# Caleb Bell
+# Extracts phone number and email address from text
+
+
+import re,pyperclip
+
+class Extractor:
+    """uses regular expressions to extract data from clipboard"""
+    def __init__(self):
+
+    def email_extract(text):
+        phone_regex = re.compile(r'\d{3}-\d{3}-\d{4}')
+
+
+
